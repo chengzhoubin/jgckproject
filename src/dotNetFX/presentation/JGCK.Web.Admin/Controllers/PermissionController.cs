ea2a514b6dc46aedbe0feb1bd5ffabebb353d576
chengzb <chengzb@dumasoftware.com>
2018-11-21 00:20:51 +0800

    角色权限管理

---
+++ src/dotNetFX/presentation/JGCK.Web.Admin/Controllers/PermissionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JGCK.Web.General;
+
+namespace JGCK.Web.Admin.Controllers
+{
+    public class PermissionController : JGCK_MvcController
+    {
+        // GET: Permission
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
