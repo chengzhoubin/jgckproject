15fdd28c7ce3f103a3035c4a0917926ca4b4efb8
chengzb <chengzb@dumasoftware.com>
2018-11-27 10:19:04 +0800

    动态注册HttpModules

---
+++ src/dotNetFX/instrument/JGCK.Web.General/HttpModules/DynamicBindServiceModules.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace JGCK.Web.General.HttpModules
+{
+    internal class DynamicBindServiceModules : IHttpModule
+    {
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.EndRequest += (o, e) =>
+            {
+                //context.Response.Write("test12");
+            };
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/dotNetFX/instrument/JGCK.Web.General/HttpModules/PreApplicationStartCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+namespace JGCK.Web.General.HttpModules
+{
+    public class PreApplicationStartCode
+    {
+        public static void PreStart()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(DynamicBindServiceModules));
+        }
+    }
+}
