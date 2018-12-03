90845102635eb63b7f29183273054313d41fc5b1
chengzb <chengzb@dumasoftware.com>
2018-12-03 15:54:52 +0800

    修改 C# 7.2语法

--- src/dotNetFX/instrument/JGCK.Web.General/MVC/JsonNetValueProviderFactory.cs
+++ src/dotNetFX/instrument/JGCK.Web.General/MVC/JsonNetValueProviderFactory.cs
@@ -45,25 +45,28 @@ namespace JGCK.Web.General.MVC
 
         private void AddToBackingStore(Dictionary<string, object> backingStore, string prefix, object value)
         {
-            if (value is IDictionary<string, object> d)
+            var d = value as IDictionary<string, object>;
+            if (d != null)
             {
                 foreach (var entry in d)
                 {
                     AddToBackingStore(backingStore, MakePropertyKey(prefix, entry.Key), entry.Value);
                 }
+
                 return;
             }
 
-            //var l = value as IList;
-            if (value is IList l)
+            var l = value as IList;
+            if (l != null)
             {
                 for (int i = 0; i < l.Count; i++)
                 {
                     AddToBackingStore(backingStore, MakeArrayKey(prefix, i), l[i]);
                 }
+
                 return;
             }
-            
+
             backingStore[prefix] = value;
         }
 
