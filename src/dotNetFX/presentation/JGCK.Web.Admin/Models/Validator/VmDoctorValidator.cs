32e8b87ca662e3336539822850d0690074e5ad2e
chengzb <chengzb@dumasoftware.com>
2018-12-03 10:56:30 +0800

    2018-12-3

---
+++ src/dotNetFX/presentation/JGCK.Web.Admin/Models/Validator/VmDoctorValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidation;
+
+namespace JGCK.Web.Admin.Models.Validator
+{
+    public class VmDoctorValidator : AbstractValidator<VmUserDoctor>
+    {
+        public VmDoctorValidator()
+        {
+            this.RuleFor(ud => ud.NagigatedDomainObject.Name)
+                .NotNull()
+                .NotEmpty().WithMessage("医生用户名不能为空");
+            this.RuleFor(ud => ud.NagigatedDomainObject.RealName)
+                .NotNull()
+                .NotEmpty().WithMessage("医生姓名不能为空");
+            this.RuleFor(ud=>ud.NagigatedDomainObject.Pwd)
+                .NotNull()
+                .NotEmpty().WithMessage("密码不能为空");
+            this.RuleFor(ud => ud.NagigatedDomainObject.Doctor.InHospital)
+                .Must(h => h?.Count > 0)
+                .WithMessage("所属医院，所属科室或者科室地址不能为空");
+            this.RuleFor(ud=>ud.NagigatedDomainObject.Doctor.DoctorCode)
+                .NotNull()
+                .NotEmpty().WithMessage("医生编号不能为空");
+            this.RuleFor(ud => ud.NagigatedDomainObject.Doctor.DoctorLicensePic)
+                .NotNull()
+                .NotEmpty().WithMessage("医师资格证不能为空");
+            //this.RuleFor(v=>v.NagigatedDomainObject.Doctor)
+        }
+    }
+}
