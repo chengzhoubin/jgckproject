b74b099df60fbaad63fbade33d39ae92dc872f1e
chengzb <chengzb@dumasoftware.com>
2018-12-06 14:21:08 +0800

    新增产品列表

---
+++ src/dotNetFX/presentation/JGCK.Web.Admin/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JGCK.Web.General;
+
+namespace JGCK.Web.Admin.Controllers
+{
+    public class ProductController : JGCK_MvcController
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/dotNetFX/presentation/JGCK.Web.Admin/Models/Mapper/VmHospitalMapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ExpressMapper;
+using JGCK.Respority.BasicInfo;
+
+namespace JGCK.Web.Admin.Models.Mapper
+{
+    public static class VmHospitalMapper
+    {
+        static VmHospitalMapper()
+        {
+            ExpressMapper.Mapper.Register<Hospital, Hospital>().Before((h, t) =>
+            {
+                t.HospitalInvoices?.Clear();
+                t.HospitalReferences?.Clear();
+            });
+        }
+
+        public static Hospital MapTo(this Hospital existHospital)
+        {
+            //ExpressMapper.Mapper.Map<Hospital,Hospital>(existHospital)
+            return null;
+        }
+    }
+}
