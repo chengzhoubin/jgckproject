207f2e26252a7c01a87ad191706f4466c7f554b7
shinyuer <35998585@qq.com>
2018-12-10 22:17:45 +0800

    12-10  .1

---
+++ src/dotNetFX/presentation/JGCK.Web.Admin/assets/pages/scripts/1.js
@@ -0,0 +1,25 @@
+{
+    "name": "Name1 ",
+        "children": [
+            {
+                "name": "Name: A",
+                "ID": 2
+            },
+            {
+                "name": "Name: B",
+                "ID": 3
+            },
+            {
+                "name": "Name: C",
+                "ID": 4
+            },
+            {
+                "name": "Name: D",
+                "ID": 5
+            },
+            {
+                "name": "Name: E",
+                "ID": 6
+            }
+        ]
+}
