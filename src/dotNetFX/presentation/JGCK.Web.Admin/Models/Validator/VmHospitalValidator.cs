087e7e1ebfd80a20a0b60fc0726bb61f18257465
chengzb <chengzb@dumasoftware.com>
2018-12-13 12:19:43 +0800

    1

--- src/dotNetFX/presentation/JGCK.Web.Admin/Models/Validator/VmHospitalValidator.cs
+++ src/dotNetFX/presentation/JGCK.Web.Admin/Models/Validator/VmHospitalValidator.cs
@@ -17,7 +17,7 @@ namespace JGCK.Web.Admin.Models
             this.RuleFor(h => h.NagigatedDomainObject.HospitalType).Must(t => t > 0).When(h => h.NagigatedDomainObject != null).WithMessage("医院属性未选择");
             this.RuleFor(h => h.NagigatedDomainObject.PaymentType).Must(t => t > 0).When(h => h.NagigatedDomainObject != null).WithMessage("付款方式未选择");
             this.RuleFor(h => h.NagigatedDomainObject.PaymentPeriod).Must(t => t > 0).When(h => h.NagigatedDomainObject != null).WithMessage("付款周期未选择");
-            this.RuleFor(h => h.NagigatedDomainObject.SaleRate).GreaterThan(0).LessThanOrEqualTo(1).WithMessage("折扣系数格式输入错误");
+            this.RuleFor(h => h.NagigatedDomainObject.SaleRate).GreaterThan(0).LessThanOrEqualTo(1).WithMessage("折扣系数格式输入错误").OverridePropertyName("折扣系数");
         }
     }
 }
