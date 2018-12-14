aa4d72a7da90d7e9addd2e6a878846e5c585c0a3
54qiuxing@163.com <54qiuxing@163.com>
2018-12-14 17:02:43 +0800

    返回产品类型树接口好了

---
+++ src/dotNetFX/presentation/JGCK.Web.Admin/Models/VmProductTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JGCK.Web.Admin.Models
+{
+    public class VmProductTree
+    {
+        public string Name { get; set; }
+
+        public long Id { get; set; }
+
+        public IList<ProductTreeChildren> children { get; set; }
+}
+
+    public class ProductTreeChildren
+    {
+        public string Name { get; set; }
+
+        public long Id { get; set; }
+    }
+}
